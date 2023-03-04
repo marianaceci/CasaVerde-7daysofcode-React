6e1a55dab8407dc1fa26a97ccaf6f0ad510e5153
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-03-04 14:55:33 -0300

    ajuste no background image

--- src/assets/pages/HomePage/index.jsx
+++ src/assets/pages/HomePage/index.jsx
@@ -2,6 +2,7 @@ import AssinaturaNewsletter from "../../components/AssinaturaNewsletter";
 import CardCentral from "../../components/CardCentral";
 import Menu from "../../components/Menu";
 import Ofertas from "../../components/Ofertas";
+import styles from './HomePage.module.scss';
 
 function HomePage() {
 
